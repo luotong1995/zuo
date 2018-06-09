ab05124dedf23bcd691b56d87407ea0bee90a33c
Chris_luo <954926501@qq.com>
2018-06-09 22:59:56 +0800

    Stack and List

---
+++ stack/Uitl/src/DoubleUtils.java
@@ -0,0 +1,9 @@
+public class DoubleUtils {
+
+    public static void main(String[] args){
+        //向上取整
+        System.out.println(Math.ceil(0.5));
+        //向下取整
+        System.out.println(Math.floor(0.5));
+    }
+}
